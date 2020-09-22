91f0aed5c4353681e5710cb1fab8a5e929eaee0e
l9c4s <lucas.luizacar22@gmail.com>
2020-09-22 16:40:38 -0300

    adicionando css da tela de cadastro com sucesso/erro

--- PHP/cadastrar.php
+++ PHP/cadastrar.php
@@ -1,6 +1,7 @@
 <html>
     <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="/../teste - PHP/css/cadastrar.css">
     <title>Teste DKMA</title>
     </head>
     <body>
@@ -19,10 +20,12 @@ $sql = "select count(*) as total from usuario where usu_name = '$nome'";
 $resultado = mysqli_query($con, $sql);
 $row = mysqli_fetch_assoc($resultado);
 if($row['total'] == 1){
-    echo "<p> O nome $nome ja existe</p>";
-    echo "<p> por favor clique em um dos botoes abaixo para retornar a pagina de cadastro ou login</p>";
+    echo "<div class= 'content'>";
+    echo "<span> O nome $nome ja existe <br></span";
+    echo "<span> por favor clique em um dos botoes abaixo para retornar a pagina de cadastro ou login</span>";
     echo "<a href = '/../teste - PHP/index.html'>login</a>";
     echo "<a href = '/../teste - PHP/cadastro.php'>cadastro </a>";
+    echo "</div>";
     exit();
 }
 ## verificando se o email ja tem cadastrado
@@ -30,19 +33,23 @@ $sql = "select count(*) as total from usuario where usu_email = '$email'";
 $resultado = mysqli_query($con, $sql);
 $row = mysqli_fetch_assoc($resultado);
 if($row['total'] == 1){
-    echo "<p> O email $email ja existe</p>";
-    echo "<p> por favor clique em um dos botoes abaixo para retornar a pagina de cadastro ou login</p>";
+    echo "<div class= 'content'>";
+    echo "<span> O email $email ja existe</span>";
+    echo "<span> por favor clique em um dos botoes abaixo para retornar a pagina de cadastro ou login</p>";
     echo "<a href = '/../teste - PHP/index.html'>login</a>";
     echo "<a href = '/../teste - PHP/cadastro.php'>cadastro </a>";
+    echo "</div>";
     exit();
 }
 ## inserindo registro na tabela.
 $insere = "INSERT INTO usuario (id_usu, usu_name, usu_senha, usu_email, usu_tel, usu_cpf, usu_rg) VALUES (NULL, '$nome', '$senha', '$email', '$tel', '$cpf', '$rg')";
 if($con->query($insere) === TRUE){
-    echo "<p> O usuario $nome foi cadastrado com sucesso</p>";
-    echo "<p> voce pode voltar para a pagina de cadastro ou login</p>";
+    echo "<div class= 'content'>";
+    echo "<span> O usuario $nome foi cadastrado com sucesso</span>";
+    echo "<span> voce pode voltar para a pagina de cadastro ou login</p>";
     echo "<a href = '/../teste - PHP/index.html'>Login</a>";
     echo "<a href = '/../teste - PHP/cadastro.php'>cadastro </a>";
+    echo "</div>";
     exit();
 }
 $con -> close();
