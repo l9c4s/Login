4601553edaebbf021f3f24fdf6c9d48f072f4879
l9c4s <lucas.luizacar22@gmail.com>
2020-09-22 00:17:01 -0300

    adicionando css tela do cadastro

--- cadastro.php
+++ cadastro.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste</title>
     <script src="/teste - PHP/js/jquery-3.3.1.min.js"></script>
     <script src="/teste - PHP/JS/Valida.js"></script>
     <script src="/teste - PHP/js/jquery.mask.min.js"></script>
+    <link rel="stylesheet" href="/../teste - PHP/css/cadastro.css">
     <script>
         //mascara de campos utilizando jquery
         $(document).ready(function(){
@@ -30,48 +30,43 @@
             display: block;
         }
     </style>
-
+<title>Teste</title>
 </head>
 <body>
-    <h1>Formulario para cadastro</h1>
+   
     <form name="form_user" onsubmit="return sendbot(this)" action="PHP/cadastrar.php" method="post" >
         <div class="content">
             <div id="cadastro">
-                <h1>Preencha os campos abaixos</h1>
+            <span>Formulario para cadastro</span>
+                <span>Preencha os campos abaixos</span>
                 <p>
-                    <label for="nome_login">Digite seu nome</label>
                     <input  id="nome"  type="text"  maxlength="30" name="nome" onkeyup="recebeNome()" onblur="validanome()" placeholder="Digite aqui o Usuario"/>
                     <span  id="testeSpan" class="valido" style="color: red;">*Este campo precisa ter 6 caracteres*</span>
                 </p>
                 <p>
-                    <label for="senha_usu" >Digite sua senha</label>
-                    <input type="password" name="senha_usu"  onkeyup="recebesenha()" onblur="validasenha()">
+                    <input type="password" name="senha_usu"  onkeyup="recebesenha()" onblur="validasenha()" placeholder="Digite aqui sua senha">
                     <span  id="testeSpan1" class="valido" style="color: red;">*Este campo precisa ter 6 caracteres*</span>
                 </p>
                 <p>
-                    <label for="email_usu">Digite seu email</label>
-                    <input type="text" name="email_usu" id="email" onkeyup="recebeemail()" onblur="validaemail()">
+                    <input type="text" name="email_usu" id="email" onkeyup="recebeemail()" onblur="validaemail()" placeholder="Digite aqui seu email">
                     <span  id="testeSpan2" class="valido" style="color: red;">*email incorreto*</span>
                 </p>
-                <p>
-                    <label for="email_usu">Telefone</label>
-                    <input type="text" name="telefone" id="telefone" onkeyup="recebetel()" onblur="validatel()">
+                <p> 
+                    <input type="text" name="telefone" id="telefone" onkeyup="recebetel()" onblur="validatel()" placeholder = "Digite aqui seu telefone">
                     <span  id="testeSpan3" class="valido" style="color: red;">*numero invalido*</span>
                 </p>
                 <p>
-                    <label for="email_usu">CPF</label>
-                    <input type="text" name="cpf_usu" id="cpf_usu" onkeyup="recebecpf()" onblur="validacpf()">
+                    <input type="text" name="cpf_usu" id="cpf_usu" onkeyup="recebecpf()" onblur="validacpf()" placeholder="Digite aqui seu CPF">
                     <span  id="testeSpan4" class="valido" style="color: red;">*CPF invalido*</span>
                 </p>
                 <p>
-                    <label for="email_usu">RG</label>
-                    <input type="text" name="rg_usu" id="rg_usu" onkeyup="receberrg()" onblur="validarg()">
+                    <input type="text" name="rg_usu" id="rg_usu" onkeyup="receberrg()" onblur="validarg()" placeholder="Digite aqui seu RG"> 
                     <span  id="testeSpan5" class="valido" style="color: red;">*RG invalido*</span>
                 </p>
                 <input type="submit" value="enviar" onclick="sendbot()">
+                <a href = '/../teste - PHP/index.html'>Login</a>
             </div>
         </div>
     </form>
-    <a href = '/../teste - PHP/index.html'>Login</a>
 </body>
 </html>
